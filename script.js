5faf9fe8d00143621648e762cde48c0c80a7e328
ChetanPrince <saini.rajvir2@gmail.com>
2024-10-13 21:31:52 +0200

    8

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ let selectedRow = null;
 function saveData(){
     let formData = getData();
     if(selectedRow === null){
-
         let inputs = document.querySelectorAll(".inputsActual");
         let allFieldsFilled = true;
-        
         inputs.forEach(input => {
             if(input.value.trim() ===""){
                 allFieldsFilled = false;
@@ -27,9 +25,6 @@ function saveData(){
     clearForm();
 }
 
-
-
-
 function getData(){
     return{
         name: document.getElementById("nameFirst").value,
@@ -45,8 +40,6 @@ function printData(formData){
     let tr  = document.createElement("tr");
     tr.innerHTML =`<td>${formData.name}</td><td>${formData.surname}</td><td>${formData.contact}</td><td>${formData.email}</td><td><button id="edit" onClick="editRow(this)">Edit</button><button id="delete" onclick="deleteRow(this)">Delete</button></td>`;
     table.appendChild(tr);
-
-    // let savedData = localStorage.getItem("employeeData") || (localStorage.setItem("employeeData", defaultData), defaultData);
 }
 
 function editRow(td){
