0f2258da09c23706e0bedefe485d43ef77400fc1
ChetanPrince <saini.rajvir2@gmail.com>
2024-10-13 21:29:56 +0200

    7

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ searchBtn.addEventListener("click", saveData);
 let selectedRow = null;
 
 function saveData(){
+    let formData = getData();
     if(selectedRow === null){
 
         let inputs = document.querySelectorAll(".inputsActual");
-        let formData = getData();
         let allFieldsFilled = true;
         
         inputs.forEach(input => {
@@ -42,7 +42,6 @@ function getData(){
 
 function printData(formData){
     let table = document.getElementById("one").getElementsByTagName("tbody")[0];
-    table.innerHTML = "";
     let tr  = document.createElement("tr");
     tr.innerHTML =`<td>${formData.name}</td><td>${formData.surname}</td><td>${formData.contact}</td><td>${formData.email}</td><td><button id="edit" onClick="editRow(this)">Edit</button><button id="delete" onclick="deleteRow(this)">Delete</button></td>`;
     table.appendChild(tr);
@@ -61,11 +60,24 @@ function editRow(td){
     console.log(selectedRow);
 
 }
-
-
 function clearForm(){
 document.getElementById("nameFirst").value = "";
     document.getElementById("secondNameFirst").value = "";
     document.getElementById("contactNo").value="";
     document.getElementById("emailId").value ="";
+}
+function deleteRow(td){
+    selectedRow = td.parentElement.parentElement;
+    if(confirm("Are you sure you want to delete the row?")){
+        selectedRow.remove();
+    }
+    selectedRow = null;
+}
+function updateData(formData){
+    console.log(formData.name)
+    selectedRow.cells[0].innerHTML = formData.name;
+    selectedRow.cells[1].innerHTML = formData.surname;
+    selectedRow.cells[2].innerHTML = formData.contact;
+    selectedRow.cells[3].innerHTML = formData.email;
+    selectedRow = null;
 }
