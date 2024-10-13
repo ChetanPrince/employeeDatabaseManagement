28cd02904ce2159b131531c5e4baa43b6b383cd6
ChetanPrince <saini.rajvir2@gmail.com>
2024-10-13 21:18:52 +0200

    1

--- script.js
+++ script.js
@@ -47,12 +47,25 @@ function printData(formData){
     tr.innerHTML =`<td>${formData.name}</td><td>${formData.surname}</td><td>${formData.contact}</td><td>${formData.email}</td><td><button id="edit" onClick="editRow(this)">Edit</button><button id="delete" onclick="deleteRow(this)">Delete</button></td>`;
     table.appendChild(tr);
 
-    let savedData = localStorage.getItem("employeeData") || (localStorage.setItem("employeeData", defaultData), defaultData);
+    // let savedData = localStorage.getItem("employeeData") || (localStorage.setItem("employeeData", defaultData), defaultData);
 }
 
 function editRow(td){
 
-    selectedRow = td.parentElement.ParentElement;
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("nameFirst").value = selectedRow.cells[0].innerHTML;
+        document.getElementById("secondNameFirst").value = selectedRow.cells[1].innerHTML;
+        document.getElementById("contactNo").value=selectedRow.cells[2].innerHTML;
+        document.getElementById("emailId").value =selectedRow.cells[3].innerHTML;
+
     console.log(selectedRow);
 
+}
+
+
+function clearForm(){
+document.getElementById("nameFirst").value = "";
+    document.getElementById("secondNameFirst").value = "";
+    document.getElementById("contactNo").value="";
+    document.getElementById("emailId").value ="";
 }
