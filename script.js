89ea6806e67d8beef82b4d1458b508059a98f136
ChetanPrince <saini.rajvir2@gmail.com>
2024-10-13 02:54:36 +0200

    prevent default action

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ searchBtn.addEventListener("click", saveData);
 
 let selectedRow = null;
 
-window.onload = saveData;
+// window.onload = saveData;
 
 function saveData(e){
     e.preventDefault();
@@ -17,8 +17,6 @@ function saveData(e){
     }
 clearForm();
 }
-
-
 
 function getData(){
     return{
@@ -34,7 +32,6 @@ function printData(formData){
     let table = document.getElementById("one").getElementsByTagName("tbody")[0];
     table.innerHTML = "";
     let tr  = document.createElement("tr");
-    console.log(table);
     tr.innerHTML =`<td>${formData.name}</td><td>${formData.surname}</td><td>${formData.contact}</td><td>${formData.email}</td><td><button id="edit" onClick="editRow(this)">Edit</button><button id="delete" onclick="deleteRow(this)">Delete</button></td>`;
     table.appendChild(tr);
 
