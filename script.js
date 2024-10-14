b82c98f9a2456c5bf0f21c75dc1ed4baaeb8af39
ChetanPrince <saini.rajvir2@gmail.com>
2024-10-14 02:07:36 +0200

    1

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ function printData(formData){
     let tr  = document.createElement("tr");
     tr.innerHTML =`<td>${formData.name}</td><td>${formData.surname}</td><td>${formData.contact}</td><td>${formData.email}</td><td><button id="edit" onClick="editRow(this)">Edit</button><button id="delete" onclick="deleteRow(this)">Delete</button></td>`;
     table.appendChild(tr);
+    
 }
 
 function editRow(td){
@@ -49,9 +50,6 @@ function editRow(td){
         document.getElementById("secondNameFirst").value = selectedRow.cells[1].innerHTML;
         document.getElementById("contactNo").value=selectedRow.cells[2].innerHTML;
         document.getElementById("emailId").value =selectedRow.cells[3].innerHTML;
-
-    console.log(selectedRow);
-
 }
 function clearForm(){
 document.getElementById("nameFirst").value = "";
