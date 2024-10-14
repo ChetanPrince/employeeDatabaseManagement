a451cc27e187a3f16bbda2aa67872ccb3f763218
ChetanPrince <saini.rajvir2@gmail.com>
2024-10-14 02:13:51 +0200

    2

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-let searchBtn = document.getElementById("submit");
-searchBtn.addEventListener("click", saveData);
-
 let selectedRow = null;
+window.onload = saveData;
 
 function saveData(){
     let formData = getData();
@@ -40,7 +38,7 @@ function printData(formData){
     let tr  = document.createElement("tr");
     tr.innerHTML =`<td>${formData.name}</td><td>${formData.surname}</td><td>${formData.contact}</td><td>${formData.email}</td><td><button id="edit" onClick="editRow(this)">Edit</button><button id="delete" onclick="deleteRow(this)">Delete</button></td>`;
     table.appendChild(tr);
-    
+
 }
 
 function editRow(td){
