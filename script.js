a9977510818aa1a92a81b384a4391bda2ec82cc8
ChetanPrince <saini.rajvir2@gmail.com>
2024-11-03 20:03:32 +0100

    updated script

--- script.js
+++ script.js
@@ -42,5 +42,8 @@ function getData(){
 }
 
 function printData(formData){
-    
+    let table = document.getElementById("one").getElementsByTagName("tbody")[0];
+    let tr =document.createElement("tr");
+    tr.innerHTML =`<td>${formData.name}</td><td>${formData.surname}</td><td>${formData.contact}</td><td>${formData.email}</td><td><button id="edit" onClick="editRow(this)">Edit</button><button id="delete" onclick="deleteRow(this)">Delete</button></td>`;
+    table.appendChild(tr);
 }
